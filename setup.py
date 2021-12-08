0f9638e984248713c7394cc0226f515d52c289ed
Cássio Eskelsen <cassio.eskelsen@pagueveloz.com.br>
2021-12-08 14:22:39 -0300

    specify azure-servicebus version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tbevents',
-      version='1.0.3',
+      version='1.0.5',
       url="https://github.com/thingsbuilder/tbevents",
       license="GPL",
       classifiers=[
@@ -15,4 +15,4 @@ setup(name='tbevents',
                   'as Event Bus',
       packages=find_packages(),
       long_description=open('README.md').read(),
-      install_requires=['kombu', 'python-dateutil', 'azure-servicebus'])
+      install_requires=['kombu', 'python-dateutil', 'azure-servicebus>=7.4.0'])
