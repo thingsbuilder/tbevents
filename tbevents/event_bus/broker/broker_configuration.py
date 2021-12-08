5e34f71556b6d4ab29d24a70ea43551434aae2ea
Fabiano Teichmann <fabiano.teichmann@pagueveloz.com.br>
2021-12-08 15:56:57 -0300

    set default provider RabbitMq if not set environ PROVIDER

--- tbevents/event_bus/broker/broker_configuration.py
+++ tbevents/event_bus/broker/broker_configuration.py
@@ -4,9 +4,9 @@ from tbevents.event_bus.broker.service_bus.sb_settings import SBSettings
 from tbevents.event_bus.broker.rabbit.rabbit_provider import RabbitMQProvider
 from tbevents.event_bus.broker.rabbit.rabbit_settings import RabbitSettings
 
-if os.environ.get("PROVIDER") == "RabbitMQ":
-    DEFAULT_BROKER_PROVIDER_CLASS = RabbitMQProvider
-    DEFAULT_BROKER_CONFIGURATION_CLASS = RabbitSettings
-else:
+if os.environ.get("PROVIDER") == "Service Bus":
     DEFAULT_BROKER_PROVIDER_CLASS = SBProvider
     DEFAULT_BROKER_CONFIGURATION_CLASS = SBSettings
+else:
+    DEFAULT_BROKER_CONFIGURATION_CLASS = RabbitSettings
+    DEFAULT_BROKER_PROVIDER_CLASS = RabbitMQProvider
